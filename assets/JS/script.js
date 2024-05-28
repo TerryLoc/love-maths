29583d05c5698d802a1f20b911efd141785ddc51
TerryLoc <terryloughran@gmail.com>
2024-05-28 11:35:54 +0100

    function for JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,16 @@
 'use strict';
+function runGame() {}
+
+function checkAnswer() {}
+
+function calCorrectAns() {}
+
+function incrementScore() {}
+
+function incrementWrongAns() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
