a68e41f90920cd5e4e4788a2c02ecf854e7bc6a4
TerryLoc <terryloughran@gmail.com>
2024-05-29 12:02:08 +0100

    add subtract & multiply functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -31,6 +31,10 @@ function runGame(gameType) {
 
   if (gameType === 'addition') {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === 'subtract') {
+    displaySubtractQuestion(num1, num2);
+  } else if (gameType === 'multiply') {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting`;
@@ -66,6 +70,10 @@ function calCorrectAns() {
 
   if (operator === '+') {
     return [operand1 + operand2, 'addition'];
+  } else if (operator === '-') {
+    return [operand1 - operand2, 'subtract'];
+  } else if (operator === 'x') {
+    return [operand1 * operand2, 'multiply'];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -89,6 +97,17 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  // If operand1 is greater than operand2, then the value of operand1 is assigned to the element's text content. Otherwise, the value of operand2 is assigned to the element's text content.
+  document.getElementById('operand1').textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById('operand2').textContent =
+    operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = '-';
+}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = 'x';
+}
